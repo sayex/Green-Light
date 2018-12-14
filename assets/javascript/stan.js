1f7cf184d90df2f656c0b7b00c5a77d670e8712c
Stan Bakalov <sunny.bakalov@gmail.com>
2018-12-13 19:48:51 -0700

    put in ajax call but nothing in there is right so far this api is very complex

--- assets/javascript/stan.js
+++ assets/javascript/stan.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+$(document).ready(function() {
+
+    var userSearch;
+    var spotifyUrl = "https://api.spotify.com";
+    var clientId = "11a98f02ba9a423a9dfbd30766f55d62";
+    var clientSecret = "0dd068ede3e9462caa99fc30db16bb07";
+
+    $.ajax({
+        url: spotifyUrl,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        var results = response.data;
+    })
+}); 
