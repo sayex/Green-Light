82cdf9ec2ec7d18f47fdc906c4c2a6057b9c6fb4
sayex <elsayer@msn.com>
2018-12-14 21:32:50 -0700

    moved popoutButton into a function

--- assets/javascript/eric.js
+++ assets/javascript/eric.js
@@ -7,9 +7,39 @@ $(document).ready(function () {
         });
     })
 
-    // ajax call to get bands in town info
-    function searchBandsInTown(artist) {
+
+    //function to create popouttext in popover
+
+    function popoutButton(response) {
         var popoverText = ""
+        for (var i = 0; i < response.length; i++) {
+            var offersUrl
+            if (response[i].offers.length !== 0) {
+                for (j = 0; j < response[i].offers.length; j++) {
+                    offersUrl = response[i].offers[j].url;
+                }
+            } else {
+                offersUrl = "no offers";
+            }
+            var lineup = response[i].lineup[0]
+            var venue = response[i].venue
+            var dateOfPerformance = moment(response[i].datetime, "YYYY-MM-DD").format("MM/DD/YYYY")
+            var venueRegion = venue.region
+            var venueCity = venue.city;
+            var venueName = venue.name;
+
+            popoverText = popoverText + `<div id="popoverText"><ul><li>${dateOfPerformance}</li><li><b> State:</b> ${venueRegion} <b>City:</b> ${venueCity} <b>Venue:</b> ${venueName}this is a test of this venu</li><li><a href="${offersUrl}" target= "_blank">Tickets</a></li></ul></div>`;
+
+        }
+
+        $("#bandPopover").text(lineup);
+        $("#bandPopover").attr("data-content", popoverText)
+    }
+
+    // ajax call to get bands in town info
+
+    function searchBandsInTown(artist) {
+
         var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=project1UT13543&date=upcoming"
 
         $.ajax({
@@ -18,30 +48,12 @@ $(document).ready(function () {
             })
             .then(function (response) {
 
-                for (var i = 0; i < response.length; i++) {
-                    var offersUrl
-                    if (response[i].offers.length !== 0) {
-                        for (j = 0; j < response[i].offers.length; j++) {
-                            offersUrl = response[i].offers[j].url;
-                        }
-                    } else {
-                        offersUrl = "no offers";
-                    }
-                    var lineup = response[i].lineup[0]
-                    var venue = response[i].venue
-                    var dateOfPerformance = moment(response[i].datetime, "YYYY-MM-DD").format("MM/DD/YYYY")
-                    var venueRegion = venue.region
-                    var venueCity = venue.city;
-                    var venueName = venue.name;
+                popoutButton(response)
 
-                    popoverText = popoverText + `<div id="popoverText"><ul><li>${dateOfPerformance}</li><li><b> State:</b> ${venueRegion} <b>City:</b> ${venueCity} <b>Venue:</b> ${venueName}this is a test of this venu</li><li><a href="${offersUrl}" target= "_blank">Tickets</a></li></ul></div>`;
-
-                }
-
-                $("#bandPopover").text(lineup);
-                $("#bandPopover").attr("data-content", popoverText)
             });
     }
+
+    searchBandsInTown("Chain Smokers")
 
     //function to intergrate musixmatch at a later date
     // function musixmatch() {
@@ -56,6 +68,5 @@ $(document).ready(function () {
     // }
     // musixmatch()
 
-    searchBandsInTown("Chain Smokers")
 
 })
