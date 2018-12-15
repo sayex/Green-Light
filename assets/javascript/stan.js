b03915826c86291c9426ac2f408cf8440d5fa9ac
Stan Bakalov <sunny.bakalov@gmail.com>
2018-12-15 11:21:38 -0700

    committing before rebasing

--- assets/javascript/stan.js
+++ assets/javascript/stan.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var userSearch;
-    var spotifyUrl = "https://api.spotify.com";
+    var spotifyUrl = "https://api.spotify.com/v1/search";
     var clientId = "11a98f02ba9a423a9dfbd30766f55d62";
     var clientSecret = "0dd068ede3e9462caa99fc30db16bb07";
 
