4560b67a8412dcc8be8911c18085ff75316be93f
sayex <elsayer@msn.com>
2018-12-18 15:17:47 -0700

    kept the var for client id and secret but took out the data

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
