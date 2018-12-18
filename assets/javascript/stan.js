8a79ace36d92f5a82b84ddff6e3fd5476f323d6e
Stan Bakalov <sunny.bakalov@gmail.com>
2018-12-17 21:23:18 -0700

    got the artist object working

--- assets/javascript/stan.js
+++ assets/javascript/stan.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
+  // "https://api.spotify.com/v1/search?q=Muse&type=track%2Cartist&market=US&limit=10&offset=5"
 
-    var userSearch;
-    var spotifyUrl = "https://api.spotify.com/v1/search";
-    var clientId = "11a98f02ba9a423a9dfbd30766f55d62";
-    var clientSecret = "0dd068ede3e9462caa99fc30db16bb07";
+    var spotifyUrl = "https://api.spotify.com/v1/search?q=";
+    var userSearch = "madonna";
+    var lastPartOfUrl = "&type=artist&market=US&limit=10&offset=0"
+
 
     $.ajax({
-        url: spotifyUrl,
-        method: "GET"
+        url: spotifyUrl + userSearch + lastPartOfUrl,
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": "Bearer BQBydjda8-JamU-7pzYHZ1xHwepu_QgH9b1xH-tGPFbdjXcClps4mSj7L2ESZJqcvL3MdZ-TBwdMdAu7OO9rHtB_MvIsYmsEpE4XbGn0WcLghkY6cTITnht0-rMMlD2MEDY5AYqef_gdF_NEJfgB5EyNeafu0TLzwYV3ebtxLfTJJT_UwSIXxQgvePs"
+        }
     }).then(function(response) {
+
         console.log(response);
 
-        var results = response.data;
+        var results = response.artists.items;
+
+        for (var i = 0; i < results.length; i++) {  
+          console.log(results[i])
+        
+        if (results[i].images.length > 0) {
+          var albumImage = $("<img class='album'>");
+          albumImage.attr("src", results[i].images[1].url);
+          $(".album").append(albumImage);
+        } else {
+          var errorImage = $("<img class='errorImage'>");
+          errorImage.attr("src", "assets/images/error.jpg");
+          $(".album").append(errorImage);
+        }
+        }
     })
 }); 
