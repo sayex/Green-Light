6fd3c2a3d3cbadb8da87f4c1301d08027d24a2fa
Stan Bakalov <sunny.bakalov@gmail.com>
2018-12-19 21:19:44 -0700

    got some of the functionality working

--- assets/javascript/stan.js
+++ assets/javascript/stan.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+
+  var access_token4 = "";
+if (window.location.href.match(/\#(?:access_token)\=([\S\s]*?)\&/) !== null) {
+    access_token4 = window.location.href.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1]
+};
+
+var authHeader = "Bearer " + access_token4;
+console.log(authHeader);
+
+console.log(access_token4);
   // "https://api.spotify.com/v1/search?q=Muse&type=track%2Cartist&market=US&limit=10&offset=5"
 
     var spotifyUrl = "https://api.spotify.com/v1/search?q=";
@@ -12,26 +22,53 @@ $(document).ready(function() {
         headers: {
           "Accept": "application/json",
           "Content-Type": "application/json",
-          "Authorization": "Bearer BQBydjda8-JamU-7pzYHZ1xHwepu_QgH9b1xH-tGPFbdjXcClps4mSj7L2ESZJqcvL3MdZ-TBwdMdAu7OO9rHtB_MvIsYmsEpE4XbGn0WcLghkY6cTITnht0-rMMlD2MEDY5AYqef_gdF_NEJfgB5EyNeafu0TLzwYV3ebtxLfTJJT_UwSIXxQgvePs"
+          "Authorization": authHeader
         }
     }).then(function(response) {
+      var results = response.artists.items;
+      var artistId = results[0].id;
+      var searchAlbumUrl = "https://api.spotify.com/v1/artists/" + artistId + "/albums?limit=10&include_groups=album"
 
-        console.log(response);
+      $.ajax({
+        url: searchAlbumUrl,
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": authHeader
+        }
+    }).then(function(responseArtist) {
+        var resultsArtist = responseArtist.items
 
-        var results = response.artists.items;
-
-        for (var i = 0; i < results.length; i++) {  
-          console.log(results[i])
+        console.log(responseArtist);
         
-        if (results[i].images.length > 0) {
-          var albumImage = $("<img class='album'>");
-          albumImage.attr("src", results[i].images[1].url);
-          $(".album").append(albumImage);
-        } else {
-          var errorImage = $("<img class='errorImage'>");
-          errorImage.attr("src", "assets/images/error.jpg");
-          $(".album").append(errorImage);
+        
+        for (var i = 0; i < resultsArtist.length; i++) {  
+        
+          var resultsAlbumUrl = resultsArtist[i].images[1].url;
+          var resultsAlbum = resultsArtist[i].external_urls.spotify;
+          var albumName = resultsArtist[i].name;
+          var albumYear = resultsArtist[i].release_date;
+
+           var newImg= `<div class=col-md-4 col-sm-6>
+            <div class=portfolio-item>
+                <a href=${resultsAlbum} ><div class=thumb>
+                    <div class=hover-effect>
+                        <div class=hover-content>
+                            <h1>${albumName}</h1>
+                            <p>Date released: ${albumYear}</p>
+                        </div>
+                    </div>
+                    <div class=image>
+                        <img src=${resultsAlbumUrl}>
+                    </div>
+                </div></a>
+            </div>
+        </div>`
+
+          $(".container-fluid").prepend(newImg);
         }
-        }
+        
     })
+  })
 }); 
